ddad3c947d4be33a2390d2b78f00089d62142b62
swefd <swefd>
2022-01-07 15:06:39 +0200

    upd

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const XHR = new XMLHttpRequest()
 
 const productGrid = document.getElementById('products-grid');
 
+let productsArray = [];
+
+
 url = "https://my-json-server.typicode.com/inesssik/Second-Group-MiddleJS/db"
 productsURL = "https://my-json-server.typicode.com/inesssik/Second-Group-MiddleJS/products"
 
@@ -14,7 +17,7 @@ XHR.send()
 
 XHR.onload = function() {
 
-
+    productsArray = XHR.response
 
     XHR.response.forEach(product => {
         productGrid.innerHTML += `
@@ -28,9 +31,55 @@ XHR.onload = function() {
        
         <a href='userProfile.html?id=${product.sellerId}'>Seller profile</a>
 
-        <button onclick="addProductToCart(1)">Buy</button>
+        <button onclick="addProductToCart(${product.productId})">Buy</button>
         </div>
     `
     });
 
+}
+
+
+//Cart
+
+let cart = [];
+let cartProd = document.getElementById('cart-products');
+
+
+
+function openCart() {
+    cartProd.classList.toggle('hide');
+}
+
+
+function addProductToCart(id) {
+    let product = productsArray.find(function(p) {
+        return p.productId == id;
+    })
+
+    cart.push(product);
+    drawCartProducts();
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function() {
+        document.getElementById('cart-button').classList.remove('active');
+    }, 500);
+}
+
+function drawCartProducts() {
+    if (cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
+    cartProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p) {
+        cartProd.innerHTML += `
+            <p><img src="${p.productPhoto}"> ${p.productName} |${p.price}$</p>
+            <hr>
+        `;
+        sum += p.productPrice;
+    });
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
 }
